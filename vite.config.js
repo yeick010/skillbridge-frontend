d6541dddd16b6321a93fecee3fec31d91c2577fd
yeison <yeisonriascos@gmil.com>
2025-04-22 16:17:23 +0200

    🚀 Clean working version

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000
-  }
-})
+});
